a2eda6aca34546750a59c979cd3cd83ca5e90947
765sanjay <sanjaysrc765@gmail.com>
2025-10-26 18:11:38 +0530

    base name is changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PortFolio/"   // 👈 use your repo name here
+  base: "/"   // 👈 use your repo name here
 })
